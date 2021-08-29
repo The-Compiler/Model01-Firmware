fec42172b3ff7ec48e8c0ba6bf1e91fb9ee2a31e
Florian Bruhin <me@the-compiler.org>
2021-08-29 17:30:41 +0200

    Initial palette handling based on tag status

--- HlwmPlugin.cpp
+++ HlwmPlugin.cpp
@@ -2,19 +2,48 @@
 #include "HlwmPlugin.h"
 #include "kaleidoscope/layers.h"
 #include "kaleidoscope/keyswitch_state.h"
+#include "kaleidoscope/plugin/LED-Palette-Theme.h"
 
 namespace kaleidoscope {
 namespace plugin {
 
-//cRGB Hlwm::color = CRGB(255, 0, 255);
-
 #define TAG_COUNT (sizeof(tagKeys) / sizeof(Key))
 static Key tagKeys[] = {Key_1, Key_2, Key_3, Key_4, Key_5, Key_6, Key_7, Key_8, Key_9, Key_0};
 static KeyAddr tagKeyAddrs[TAG_COUNT];
+static char tagStatus[TAG_COUNT];
+
+#define TAG_CHAR_EMPTY '.'
+#define TAG_CHAR_USED ':'
+#define TAG_CHAR_FOCUSED '#'
+#define TAG_CHAR_UNFOCUSED '-'
+#define TAG_CHAR_URGENT '!'
+// we should never see + and % if we use tag_status without specifying a monitor
+
+#define COLOR_IDX_EMPTY 0
+#define COLOR_IDX_USED 1
+#define COLOR_IDX_FOCUSED 2
+#define COLOR_IDX_UNFOCUSED 3
+#define COLOR_IDX_URGENT 4
+#define COLOR_IDX_UNKNOWN 5
 
 void Hlwm::setTagColor(uint8_t tagIndex) {
     KeyAddr keyAddr = tagKeyAddrs[tagIndex];
-    cRGB color = CRGB(tagIndex * 25, 0, 0);
+    uint8_t colorIdx = COLOR_IDX_UNKNOWN;
+
+    switch (tagStatus[tagIndex]) {
+        case TAG_CHAR_EMPTY:
+            colorIdx = COLOR_IDX_EMPTY;
+        case TAG_CHAR_USED:
+            colorIdx = COLOR_IDX_USED;
+        case TAG_CHAR_FOCUSED:
+            colorIdx = COLOR_IDX_FOCUSED;
+        case TAG_CHAR_UNFOCUSED:
+            colorIdx = COLOR_IDX_UNFOCUSED;
+        case TAG_CHAR_URGENT:
+            colorIdx = COLOR_IDX_URGENT;
+    }
+
+    cRGB color = LEDPaletteTheme::lookupPaletteColor(colorIdx);
     ::LEDControl.setCrgbAt(KeyAddr(keyAddr), color);
 }
 
@@ -27,6 +56,7 @@ EventHandlerResult Hlwm::onSetup(void) {
             }
         }
     }
+    memset(tagStatus, '?', TAG_COUNT);
     return EventHandlerResult::OK;
 }
 
