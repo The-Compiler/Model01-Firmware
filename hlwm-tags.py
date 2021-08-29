548b2a5579fc23a782ba038da2096fe12b243ae0
Florian Bruhin <me@the-compiler.org>
2021-08-29 22:38:09 +0200

    More resilient serial connection

--- hlwm-tags.py
+++ hlwm-tags.py
@@ -1,7 +1,9 @@
 import subprocess
+import glob
 import serial
 
-def update_tags(ser):
+
+def update_tags():
     proc = subprocess.run(
         ['herbstclient', 'tag_status'],
         check=True,
@@ -9,12 +11,20 @@ def update_tags(ser):
         text=True,
     )
     status = [tag[0] for tag in proc.stdout.split('\t') if tag]
-    command = f"hlwm.tagstatus {''.join(status)}\n"
-    print(command)
-    ser.write(command.encode('ascii'))
+    write_command(f"hlwm.tagstatus {''.join(status)}\n")
 
 
-def hc_idle(ser):
+def write_command(command):
+    print(command.strip())
+    dev = find_device()
+    if dev is None:
+        return
+
+    with serial.Serial(dev, 9600, timeout=1) as ser:
+        ser.write(command.encode('ascii'))
+
+
+def hc_idle():
     hooks = ['tag_changed', 'tag_flags']
     proc = subprocess.Popen(
         ['herbstclient', '--idle', '|'.join(hooks)],
@@ -22,12 +32,17 @@ def hc_idle(ser):
     )
     while (line := proc.stdout.readline()):
         print(line.decode('utf-8'))
-        update_tags(ser)
-        
-    
+        update_tags()
+
+
+def find_device():
+    devices = glob.glob("/dev/serial/by-id/usb-Keyboardio_*")
+    if len(devices) != 1:
+        print(f"Didn't find suitable device: {devices}")
+        return None
+    return devices[0]
 
 
 if __name__ == '__main__':
-    ser = serial.Serial("/dev/ttyACM0", 9600, timeout=1)
-    update_tags(ser)
-    hc_idle(ser)
+    update_tags()
+    hc_idle()
